bf237463f2c302ecbdc1106f27ecf9391ea79b73
Jyoti <email.jyotisingh13@gmail.com>
2022-12-29 12:08:57 +0530

    inital commit

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ let wordCount = document.getElementById('wordCount');
 let button = document.getElementById('button');
 
 
-button.addEventListener('click', () =>{
+// button.addEventListener('click', () =>{
+//     console.log(evaluatedText.value);
+//     const arr = evaluatedText.value.split(" ");
+//     console.log(arr);
+//     console.log(arr.length);
+//     wordCount.innerText = arr.length;
+// })
+
+function countWords(){
     console.log(evaluatedText.value);
     const arr = evaluatedText.value.split(" ");
     console.log(arr);
     console.log(arr.length);
     wordCount.innerText = arr.length;
-})
+}
 
 
