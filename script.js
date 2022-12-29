d350efc1db0970c7ef09549fdd7984340508d1af
Jyoti <email.jyotisingh13@gmail.com>
2022-12-29 12:39:35 +0530

    completed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let evaluatedText = document.getElementById('evaluatedText');
 let wordCount = document.getElementById('wordCount');
 let button = document.getElementById('button');
 
+
 function countWords(){
     if(evaluatedText.value === ""){
         console.log("0");
@@ -12,6 +13,7 @@ function countWords(){
         const arr = evaluatedText.value.split(" ");
         console.log(evaluatedText.value);
         if(arr.length > 0){
+            console.log(evaluatedText.value.length);
             console.log(arr.length);
             wordCount.innerText = arr.length;
         }
