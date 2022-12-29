bbc46320f0077853c6dcd15f0cd061ad1089cc9e
Jyoti <email.jyotisingh13@gmail.com>
2022-12-29 12:13:25 +0530

    completed

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ let evaluatedText = document.getElementById('evaluatedText');
 let wordCount = document.getElementById('wordCount');
 let button = document.getElementById('button');
 
-
-// button.addEventListener('click', () =>{
-//     console.log(evaluatedText.value);
-//     const arr = evaluatedText.value.split(" ");
-//     console.log(arr);
-//     console.log(arr.length);
-//     wordCount.innerText = arr.length;
-// })
-
 function countWords(){
     console.log(evaluatedText.value);
     const arr = evaluatedText.value.split(" ");
