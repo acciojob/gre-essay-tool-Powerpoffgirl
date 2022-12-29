d96b11f19476be079d713965d5351c61316e9e56
Jyoti <email.jyotisingh13@gmail.com>
2022-12-29 11:59:33 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+let evaluatedText = document.getElementById('evaluatedText');
+let wordCount = document.getElementById('wordCount');
+let button = document.getElementById('button');
+
+
+button.addEventListener('click', () =>{
+    console.log(evaluatedText.value);
+    const arr = evaluatedText.value.split(" ");
+    console.log(arr);
+    console.log(arr.length);
+    wordCount.innerText = arr.length;
+})
+
+
