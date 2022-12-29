b7fd460e08452c31bf6e719da508918f60c17928
Jyoti <email.jyotisingh13@gmail.com>
2022-12-29 12:46:43 +0530

    complted

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function countWords(){
         wordCount.innerText = 0;
     }
     else{
-        const arr = evaluatedText.value.split(" ");
+
+        const arr = evaluatedText.value.trim().split(" ");
         console.log(evaluatedText.value);
         if(arr.length > 0){
             console.log(evaluatedText.value.length);
